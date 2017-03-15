705bedc9ea759089a68c6d9766aa06966290f60d
Yannick Heinrich <yannick.heinrich@gmail.com>
2017-03-15 17:25:03 +0100

    Fix tests.

--- auth0_test.go
+++ auth0_test.go
@@ -1,37 +1,37 @@
 package auth0
 
 import (
+	"errors"
 	"fmt"
 	"gopkg.in/square/go-jose.v2"
+	"gopkg.in/square/go-jose.v2/jwt"
 	"net/http"
 	"testing"
 )
 
-
 var secretProvider = NewKeyProvider([]byte("secret"))
+
+func validConfiguration(configuration Configuration, tokenRaw string) error {
+	validator := NewValidator(configuration)
+	headerTokenRequest, _ := http.NewRequest("", "http://localhost", nil)
+	headerValue := fmt.Sprintf("Bearer %s", tokenRaw)
+	headerTokenRequest.Header.Add("Authorization", headerValue)
+
+	_, err := validator.ValidateRequest(headerTokenRequest)
+	return err
+}
 
 func TestValidatorFull(t *testing.T) {
 
 	configuration := NewConfiguration(secretProvider, "audience", "issuer", jose.HS256)
-	validator := NewValidator(configuration)
-	headerTokenRequest, _ := http.NewRequest("", "http://localhost", nil)
-	headerValue := fmt.Sprintf("Bearer %s", tokenRaw)
-
-	// Valid token
-	headerTokenRequest.Header.Add("Authorization", headerValue)
-
-	_, err := validator.ValidateRequest(headerTokenRequest)
+	err := validConfiguration(configuration, tokenRaw)
 
 	if err != nil {
 		t.Error(err)
 	}
 
-	// Invalid token
-	// Default JWT.io token
-	invalidToken := `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdWJqZWN0IiwiaXNzIjoiaXNzdWVyIiwiYXVkIjoiYXVkaWVuY2UifQ.XjWtlyDjBoFDREk1WbvxriSdLve5jI7uyamzCiGdg9U`
-	headerTokenRequest.Header.Set("Authorization", invalidToken)
-
-	_, err = validator.ValidateRequest(headerTokenRequest)
+	invalidToken := tokenRaw + `wefwefwef`
+	err = validConfiguration(configuration, invalidToken)
 
 	if err == nil {
 		t.Error("Should be considered as invalid token")
@@ -40,24 +40,16 @@ func TestValidatorFull(t *testing.T) {
 func TestValidatorEmpty(t *testing.T) {
 
 	configuration := NewConfiguration(secretProvider, "", "", jose.HS256)
-	validator := NewValidator(configuration)
-	headerTokenRequest, _ := http.NewRequest("", "http://localhost", nil)
 	validToken := `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ`
-	headerValue := fmt.Sprintf("Bearer %s", validToken)
 
-	headerTokenRequest.Header.Set("Authorization", headerValue)
-	_, err := validator.ValidateRequest(headerTokenRequest)
+	err := validConfiguration(configuration, validToken)
 
 	if err != nil {
 		t.Error(err)
 	}
-	// Invalid token
-	// Default JWT.io token
-	otherValidToken := `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkcWRxd2Rxd2Rxd2RxIiwibmFtZSI6ImRxd2Rxd2Rxd2Rxd2Rxd2QiLCJhZG1pbiI6ZmFsc2V9.-MZNG6n5KtLIG4Tsa6oi25zZK5oadmrebS-1r1Ln82c`
-	headerValue = fmt.Sprintf("Bearer %s", otherValidToken)
-	headerTokenRequest.Header.Set("Authorization", headerValue)
 
-	_, err = validator.ValidateRequest(headerTokenRequest)
+	otherValidToken := `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkcWRxd2Rxd2Rxd2RxIiwibmFtZSI6ImRxd2Rxd2Rxd2Rxd2Rxd2QiLCJhZG1pbiI6ZmFsc2V9.-MZNG6n5KtLIG4Tsa6oi25zZK5oadmrebS-1r1Ln82c`
+	err = validConfiguration(configuration, otherValidToken)
 
 	if err != nil {
 		t.Error(err)
@@ -68,27 +60,58 @@ func TestValidatorEmpty(t *testing.T) {
 func TestValidatorPartial(t *testing.T) {
 
 	configuration := NewConfiguration(secretProvider, "required", "", jose.HS256)
-	validator := NewValidator(configuration)
-	headerTokenRequest, _ := http.NewRequest("", "http://localhost", nil)
 	validToken := `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ`
-	headerValue := fmt.Sprintf("Bearer %s", validToken)
-
-	headerTokenRequest.Header.Set("Authorization", headerValue)
-	_, err := validator.ValidateRequest(headerTokenRequest)
+	err := validConfiguration(configuration, validToken)
 
 	if err == nil {
 		t.Error("Should have failed")
 	}
-	// Invalid token
-	// Default JWT.io token
 	otherValidToken := `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkcWRxd2Rxd2Rxd2RxIiwibmFtZSI6ImRxd2Rxd2Rxd2Rxd2Rxd2QiLCJhZG1pbiI6ZmFsc2V9.-MZNG6n5KtLIG4Tsa6oi25zZK5oadmrebS-1r1Ln82c`
-	headerValue = fmt.Sprintf("Bearer %s", otherValidToken)
-	headerTokenRequest.Header.Set("Authorization", headerValue)
-
-	_, err = validator.ValidateRequest(headerTokenRequest)
+	err = validConfiguration(configuration, otherValidToken)
 
 	if err == nil {
 		t.Error("Should have failed")
+	}
+}
+
+func invalidProvider(req *http.Request) (interface{}, error) {
+	return nil, errors.New("simple error")
+}
+func TestInvalidProvider(t *testing.T) {
+
+	provider := SecretProviderFunc(invalidProvider)
+	configuration := NewConfiguration(provider, "required", "", jose.HS256)
+
+	err := validConfiguration(configuration, tokenRaw)
+
+	if err == nil {
+		t.Error("Should failed")
+	}
+}
+
+func TestClaims(t *testing.T) {
+
+	configuration := NewConfiguration(secretProvider, "audience", "issuer", jose.HS256)
+	validator := NewValidator(configuration)
+
+	headerTokenRequest, _ := http.NewRequest("", "http://localhost", nil)
+	headerValue := fmt.Sprintf("Bearer %s", tokenRaw)
+
+	// Valid token
+	headerTokenRequest.Header.Add("Authorization", headerValue)
+	_, err := validator.ValidateRequest(headerTokenRequest)
+
+	if err != nil {
+		t.FailNow()
+	}
+
+	claims := map[string]interface{}{}
+	tok, _ := jwt.ParseSigned(string(tokenRaw))
+
+	err = validator.Claims(headerTokenRequest, tok, &claims)
+
+	if err != nil {
+		t.Error("Should have decode claims correctly")
 	}
 
 }
